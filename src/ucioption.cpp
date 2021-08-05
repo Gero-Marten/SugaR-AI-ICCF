4f41902f478a5c53df6d51546c09d55a8b21b1c4
Marco Zerbinati <zerbinati.marco@gmail.com>
2021-08-05 19:50:00 +0200

    fix bug

--- src/ucioption.cpp
+++ src/ucioption.cpp
@@ -113,6 +113,7 @@ void init(OptionsMap& o) {
   o["Experience Book Min Depth"]       << Option(27, EXP_MIN_DEPTH, 64);
   o["Experience Book Max Moves"]       << Option(16, 1, 100);
   o["Variety"]                         << Option(0, 0, 40);
+  o["Use NNUE"]                        << Option(true, on_use_NNUE);
   o["EvalFile"]                        << Option(EvalFileDefaultName, on_eval_file);
 }
 
